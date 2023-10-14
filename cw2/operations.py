a2631d1f613f894c9809ba1717d75d768b72e91a
Mikołaj Pacek <mikolaj.pacek@bpower2.com>
2023-10-14 14:12:35 +0200

    added tests

--- cw2/operations.py
+++ cw2/operations.py
@@ -1,2 +1,28 @@
 def first_character(string):
     return string[0]
+
+
+def first_two_characters(string):
+    if len(string) < 2:
+        return ''
+    return string[:2]
+
+
+def all_characters_except_first_two(string):
+    return string[2:]
+
+
+def penultimate_character(string):
+    if len(string) < 2:
+        return ''
+    return string[-2]
+
+
+def last_three_characters(string):
+    if len(string) < 2:
+        return ''
+    return string[-3:]
+
+
+def all_characters_in_even_positions(string):
+    return string[::2]
